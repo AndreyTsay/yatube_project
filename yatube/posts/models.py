353979273cdced5242a33467e259ab1a3499009f
Андрей Цай <skaeris@yandex.ru>
2023-03-05 23:31:32 +0300

    Yeah

--- yatube/posts/models.py
+++ yatube/posts/models.py
@@ -3,6 +3,13 @@ from django.contrib.auth import get_user_model
 
 User = get_user_model()
 
+class Group(models.Model):
+    title = models.CharField(max_length=200)
+    slug = models.SlugField(unique=True)
+    description = models.TextField()
+
+    def __str__(self) -> str:
+        return self.title
 
 class Post(models.Model):
     text = models.TextField()
@@ -11,4 +18,12 @@ class Post(models.Model):
         User,
         on_delete=models.CASCADE,
         related_name='posts'
-    ) 
+) 
+    group = models.ForeignKey(
+        Group,
+        blank=True,
+        null=True,
+        on_delete=models.CASCADE,  
+        related_name='groups'
+    )
+
